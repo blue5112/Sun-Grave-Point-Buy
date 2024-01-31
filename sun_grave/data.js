df8ecfa85457ddb194c9a98a7894944a5a239685
blue5112 <reliant511@yahoo.com>
2024-01-30 18:00:27 -0800

    Update data.js

--- sun_grave/data.js
+++ sun_grave/data.js
@@ -11,7 +11,7 @@ const races = [
 			{
 				modType: modifierTypes.custom,
 				name: "Custom",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: ""
 			}
 		]
 	},
@@ -22,55 +22,61 @@ const races = [
 				modType: modifierTypes.preset,
 				mods: [1,1,1,1,1,1],
 				name: "Human",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			},
 			{
 				modType: modifierTypes.choose,
 				mods: null,
 				name: "Variant Human",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			},
 			{
 				modType: modifierTypes.preset,
 				mods: [0,2,0,0,1,0],
 				name: "Goblin",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			},
 			{
 				modType: modifierTypes.preset,
 				mods: [0,0,1,0,2,0],
 				name: "Kobold",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			},
 			{
 				modType: modifierTypes.choose,
 				mods: null,
 				name: "Small-Folk",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			},
 			{
 				modType: modifierTypes.preset,
 				mods: [1,0,2,0,0,0],
 				name: "Beasthide Shifter",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			},
 			{
 				modType: modifierTypes.preset,
 				mods: [2,1,0,0,0,0],
 				name: "Longtooth Shifter",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			},
       {
 				modType: modifierTypes.preset,
 				mods: [0,2,0,0,0,1],
 				name: "Swiftstride Shifter",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			},
       {
 				modType: modifierTypes.preset,
 				mods: [0,1,0,0,2,0],
 				name: "Wildhunt Shifter",
-				link: "https://docs.google.com/document/d/1iSM_3GecJTVP_LTpMUe9PG_P9LaymFCcUAw5Y45jsTg/"
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
+			},
+	{
+				modType: modifierTypes.preset,
+				mods: [0,1,0,0,0,2],
+				name: "Feyling",
+				link: "https://docs.google.com/document/d/1MqNoOkjSygbfTCKe6G-1aMe190fEsgs7pLHLBcOptnY/"
 			}
 		]
 	}
